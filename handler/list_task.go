2e5f7d736859fd2e426720f8c71708aa379735f8
takaak-endo <t.endo@branu.jp>
2023-02-23 23:20:19 +0900

    add taskとlist task処理でmysqlを利用するように

--- handler/list_task.go
+++ handler/list_task.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/endot1231/go_todo_app/entity"
 	"github.com/endot1231/go_todo_app/store"
+	"github.com/jmoiron/sqlx"
 )
 
 type ListTask struct {
-	Store *store.TaskStore
+	DB   *sqlx.DB
+	Repo *store.Repository
 }
 
 type task struct {
@@ -19,7 +21,12 @@ type task struct {
 
 func (lt *ListTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	tasks := lt.Store.All()
+	tasks, err := lt.Repo.ListTasks(ctx, lt.DB)
+	if err != nil {
+		RespondJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+	}
 	rsp := []task{}
 	for _, t := range tasks {
 		rsp = append(rsp, task{
